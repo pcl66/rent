08707fdc0c401f85a0c5409e66010a30142afe0e
1500371773@qq.com <1500371773@qq.com>
2024-03-16 13:59:19 +0800

    城市选择返回路由

--- src/pages/CityList/CityList.jsx
+++ src/pages/CityList/CityList.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './index.scss'
 import {LeftOutline} from 'antd-mobile-icons'
+import { useNavigate } from 'react-router-dom'
 
 export default function CityList() {
+  const nav = useNavigate()
   return (
     <div className='city-list'>
       <div className='city-list-top'>
-        <LeftOutline className='left-arrow'/>
+        <LeftOutline onClick={() => { nav('/home/index') }} className='left-arrow'/>
         <span>城市选择</span>
       </div>
       <div className='city-list-content'></div>
